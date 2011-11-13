d8d7da2915a3cfaa7d9ff1f77e4ba1c0d639d8c4
yiloupku@gmail.com <yiloupku@gmail.com@181b9a58-03e0-e83a-7750-a867e77afb61>
2011-11-13 01:55:10 +0000

    updated version

--- trunk/TSV/data_arrange.pl
+++ trunk/TSV/data_arrange.pl
@@ -1,49 +1,55 @@
-#! /usr/bin/perl
+#! /home/gnu/bin/perl
 # measurement flow
-# data_process.pl input_file output_file
-
+# input format input.mt0 output.mt0 -h
+# $"="\t";
 use strict;
 use warnings;
 use File::Path;
 
+	
 my @data;
-
-
 my $file=shift(@ARGV);
 my $write=shift(@ARGV);
+my $headline=0;
+if ($#ARGV != -1 && $ARGV[0] =~ /^-h$/ )
+{
+	$headline=1;
+}
+
 #my $n=shift(@ARGV);
 #done: parameter setting
 
-my $dir = `pwd`;
-chop($dir);
-my $result="${dir}/${file}";
-open HEADER, "$result" or die "Cannot open $result to read $!\n";
+open HEADER, "$file" or die "Cannot open $file to read $!\n";
 @data = <HEADER>;
 close (HEADER);
 
-#for (my $ct=0; $ct<$n; $ct++)
-my $n=-1;
-while (!($data[0] =~ /alter/))
+my $top="$data[0]  \t$data[1]";
+$top =~ s/\n//g;
+my $hd='';
+my $n=0;
+my $ii=2;
+while ($data[$ii-1] !~ /alter/)
 {
-	shift(@data);
+	$hd=$hd.$data[$ii++];
 	$n++;
 }
-shift(@data);
+$hd =~ s/\n//g;
+$hd =~ s/^\s|\s$//g;
+my @hhh=split(/\s+/,$hd);
+pop(@hhh);
+
+my $hhout=&printline(@hhh);
 
 my $ct=1;
-my $temp;
+my $temp='';
 my @output;
 
-
-foreach my $line (@data)
+foreach my $line (@data[$ii..$#data])
 {
 	my $dt=$line;
-	chop($dt);
 	$temp=$temp.$dt;
-	
 	if ($ct == $n)
 	{	
-		$temp=$temp."\n";
 		push(@output, $temp);
 		$temp='';
 		$ct=0;
@@ -54,22 +60,32 @@ foreach my $line (@data)
 foreach my $line (@output)
 {
 	my $aa=$line;
-	$aa =~ s/^(\s+)//g;
-	$aa =~ s/(\s+)/\t/g;
-	$aa =~ s/failed/1e2/g;
-	my @aaa=split(/\t/, $aa);
+	$aa =~ s/\n//g;
+	$aa =~ s/^\s*|\s*$//g;
+	$aa =~ s/failed/1e5/g;
+	my @aaa=split(/\s+/, $aa);
 	pop(@aaa);
-	pop(@aaa);
-	$"="\t";
-	$aa="@aaa";
-	$aa=$aa."\n";
-	$line=$aa;
+	if ($#aaa != $#hhh)	{	print "Error : number of items not match with header! $aa\n";	}
+	$line=&printline(@aaa);
 }
 
-
+if ($headline)
+{
+	unshift(@output, "$hhout");
+	unshift(@output, "$top\n");
+}
 
 open FILE, ">$write" or die "Cannot open $write to write $!\n";
 print FILE @output;
 close (FILE);
-print "Output $write done!\n";
+# print "Output $write done!\n";
 
+sub printline
+{
+	my $out='';
+	foreach (@_)
+	{
+		$out=$out.sprintf("%-16s","$_");
+	}
+	return("$out\n");
+}
